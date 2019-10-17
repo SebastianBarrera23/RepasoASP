b9434aa35053691f82708a3fafe84a1061e7684c
sebastianbarreraramirez <sebastianbarreraramirez@gmail.com>
2019-10-17 17:06:07 -0500

    Creacion WebForm1

---
+++ EstadoVariableWeb/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EstadoVariableWeb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private int numero = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtNumero.Text = "0";
+            }
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (ViewState["conteo"] !=null)
+            {
+                numero = (int)ViewState["conteo"] + 1;
+                txtNumero.Text = numero.ToString();
+                
+            }
+            ViewState["conteo"] = numero;
+
+        }
+    }
+}
